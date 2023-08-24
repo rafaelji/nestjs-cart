6dbb5ee1b3672ddb41068a1e8ab584aeab67c703
Rafael Barbosa Silva <r.ji@outlook.com>
2023-08-24 19:51:47 +0100

    Set api port through environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { CartModule } from './cart/cart.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule, CartModule],
+  imports: [ConfigModule.forRoot(), ProductModule, CartModule],
   controllers: [],
   providers: [],
 })
