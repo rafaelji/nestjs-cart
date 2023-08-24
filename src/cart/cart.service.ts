27140de42c796c601bbfc0bd7924d06ed6efe21b
Rafael Barbosa Silva <r.ji@outlook.com>
2023-08-24 19:14:18 +0100

    Add comments and code refactor

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -15,6 +15,7 @@ export class CartService {
   constructor(private productService: ProductService) {}
   async getCart(): Promise<Cart> {
     try {
+      // Get current cart data
       return this.getCurrentCartData();
     } catch (error) {
       console.error(error);
@@ -22,17 +23,16 @@ export class CartService {
   }
 
   async addItem(productId: string): Promise<Cart> {
+    // Throw exception if productId empty/null/undefined
     if (!productId) throw new BadRequestException('productId cannot be empty.');
 
-    const products = await this.productService.getAll();
-    const selectedProduct = products.find(
-      (product) => `${product.id}` === productId,
-    );
+    // Select product from the database by product id
+    const selectedProduct = await this.productService.getProductById(productId);
 
-    if (!selectedProduct) throw new NotFoundException('productId not found.');
-
+    // Get current cart data
     const currentCartData: Cart = await this.getCurrentCartData();
 
+    // Add selected product to the cart items list
     currentCartData.cartItems.push({
       id: selectedProduct.id,
       title: selectedProduct.title,
@@ -41,44 +41,58 @@ export class CartService {
       option2: selectedProduct.variants[0].option2,
     });
 
+    // Calculate the total of all products in the cart
     currentCartData.total = currentCartData.cartItems.reduce(
       (previous, current) => previous + parseFloat(current.price),
       0,
     );
 
+    // Update cart list on database
     await this.updateCartData(currentCartData);
     return currentCartData;
   }
 
   async removeItem(productId: string): Promise<Cart> {
+    // Throw exception if productId empty/null/undefined
     if (!productId) throw new BadRequestException('productId cannot be empty.');
 
+    // Get current cart data
     const currentCartData: Cart = await this.getCurrentCartData();
+    // Search for product inside cart's item list
     const itemIndex = currentCartData.cartItems.findIndex(
       (item) => `${item.id}` === productId,
     );
 
+    // If the product isn't found, throw exception
     if (itemIndex === -1)
       throw new NotFoundException('Item not found on cart.');
 
+    // Recalculate cart total
     currentCartData.total =
       currentCartData.total -
       parseFloat(currentCartData.cartItems[itemIndex].price);
+
+    // Remove product from cart's item list
     currentCartData.cartItems.splice(itemIndex, 1);
+    // Update cart data
     await this.updateCartData(currentCartData);
     return currentCartData;
   }
 
   async getCurrentCartData(): Promise<Cart> {
+    // check if cart's json file exists
     if (await this.isCartDataAvailable()) {
+      // if it exists, get data from the json file
       return this.getCartData();
     }
+    // if it doesn't exist, create a file with empty cart to avoid errors
     return this.createEmptyRecord();
   }
 
   async createEmptyRecord(): Promise<Cart> {
     try {
       const emptyRecord: Cart = { cartItems: [], total: 0 };
+      // Create file with empty cart
       await this.updateCartData(emptyRecord);
       return emptyRecord;
     } catch (error) {
@@ -88,9 +102,11 @@ export class CartService {
 
   async isCartDataAvailable(): Promise<boolean> {
     try {
+      // Check if the file exists. In case it doesn't exist, an error will be thrown
       await fsPromises.access(this.filePath);
       return true;
     } catch (error) {
+      // check if the error thrown was because of the non-existence of the file
       if (error.code === 'ENOENT') {
         return false;
       }
@@ -99,11 +115,13 @@ export class CartService {
   }
 
   async getCartData(): Promise<Cart> {
+    // Read content from json file
     const data = await fsPromises.readFile(this.filePath, 'utf-8');
     return JSON.parse(data);
   }
 
   async updateCartData(updatedData: Cart): Promise<void> {
+    // Write content on json file
     const convertedData = JSON.stringify(updatedData, null, 2);
     await fsPromises.writeFile(this.filePath, convertedData, 'utf-8');
   }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as data from './product-list.json';
 import { Product } from './interfaces/product.interface';
 import { Order, Parameter } from './enums/common';
@@ -10,9 +10,12 @@ export class ProductService {
     order: Order = Order.ASC,
   ): Promise<Product[]> {
     try {
+      // read data from json file
       const products = data.products as Product[];
 
+      // check if query parameter param is 'title'
       if (param === Parameter.Title) {
+        // sort result based on parameter order by asc or desc
         return [...products].sort((prodA, prodB) =>
           order === Order.ASC
             ? prodA.title.localeCompare(prodB.title)
@@ -20,7 +23,9 @@ export class ProductService {
         );
       }
 
+      // check if query parameter param is 'price'
       if (param === Parameter.Price) {
+        // sort result based on parameter order by asc or desc
         return [...products].sort((prodA, prodB) => {
           const prodAPrice = parseFloat(prodA.variants[0].price);
           const prodBPrice = parseFloat(prodB.variants[0].price);
@@ -35,5 +40,19 @@ export class ProductService {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  async getProductById(productId: string): Promise<Product> {
+    // Get all products
+    const products = await this.getAll();
+    // Find product by id
+    const selectedProduct = products.find(
+      (product) => `${product.id}` === productId,
+    );
+
+    // If product is not found throw exception
+    if (!selectedProduct) throw new NotFoundException('productId not found.');
+
+    return selectedProduct;
   }
 }
